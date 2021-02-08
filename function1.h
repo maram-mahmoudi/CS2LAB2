056eea962a3c22280694fa53ec449368d87b70ee
ikramerekabi <ikrame.rekabi@aucegypt.edu>
2021-02-09 00:26:18 +0200

    adding function1.h

---
+++ function1.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+void printSolution (int** board, int N);
+bool isSafe(int ** board,int N, int row, int queens);
+bool placeNqueens(int** board, int N, int queens);
+bool SolveNqueens(int **board, int N, int queens);
